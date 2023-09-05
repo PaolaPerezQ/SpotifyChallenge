9dde9dd6cc661c6402fcda034c6df9296ae20800
PaolaPerezQ <109629563+PaolaPerezQ@users.noreply.github.com>
2023-09-05 14:35:51 -0300

    add project

---
+++ Spotify Challenge/Presentation/Spotify_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_ChallengeApp.swift
+//  Spotify Challenge
+//
+//  Created by Pichardo Perez on 05-09-23.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
